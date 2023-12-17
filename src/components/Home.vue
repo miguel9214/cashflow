3a70ba232d816dad2a19a65abf40b1611d3f5eb0
Miguel Ramos <miguel921433@gmail.com>
2023-12-17 14:20:37 -0500

    curso terminado

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -103,16 +103,37 @@ export default {
         }, 0);
       });
     },
+    totalAmount() {
+      return this.movements.reduce((suma, m) => {
+        return suma + m.amount;
+      }, 0);
+    }
+  },
+  mounted() {
+    const movements = JSON.parse(localStorage.getItem("movements"));
+    console.log(movements);
+    if (Array.isArray(movements)) {
+      this.movements = movements.map(m => {
+        return { ...m, time: new Date(m.time) };
+      });
+    }
+
+
   },
 
   methods: {
     create(movement) {
       this.movements.push(movement);
+      this.save();
     },
     remove(id) {
       const index = this.movements.findIndex((m) => m.id === id);
       this.movements.splice(index, 1);
+      this.save();
     },
+    save(){
+      localStorage.setItem("movements",JSON.stringify(this.movements));
+    }
   },
 };
 </script>
